4a64af5f915221083fda72702633e30d25c5eeb5
Otis Singfield <singfieldotis@gmail.com>
2021-04-05 14:28:07 -0400

    first draft of pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Site opens
+// User presented with a modal
+// Modal does age verification
+// If user is under age of 21, app runs without drink recipe
+// If uuser is over 21 app runs with drink recipe 
+// When app runs user is able to select movie based on genre, year it was made, and (?)
+// Based on movie selections, a random food recipe and drink recipe are also given
+// Create a function to select the movie
+// Create a function to asign that movie a random value
+// Associate the random value with types of cuisine
+// Associate random value with drink recipe selection
+// Selections can be made for drink with user input as well
+// selections can be made for food with user input as well
+
+// Possible upgrades: drink selecction based on users age
