0693603c2760f17d1dc49cb0e0d85a2089757f9c
Justin Puzey <justpuzey@gmail.com>
2021-04-06 08:03:03 -0400

    add title and thumbnail using fetch response

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,15 +1,19 @@
 var mealID = '52940'
+var mealTitle = document.querySelector('#meal-title')
 var mealThumb = document.querySelector('#meal-img')
+
 var mealDetailURL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealID
 fetch(mealDetailURL)
   .then(function (response) {
     response.json().then(function (mealDetails) {
       console.log('details', mealDetails)
       //Add meal title
+      var mealAPITitle = mealDetails.meals[0].strMeal;
+      console.log(mealAPITitle)
+      mealTitle.textContent = mealAPITitle;
 
       //Add thumbnail image
       var thumbURL = mealDetails.meals[0].strMealThumb
-      console.log('img src', thumbURL)
       var thumbEl = document.createElement('img')
       thumbEl.setAttribute("src", thumbURL)
       thumbEl.setAttribute("class", 'thumbnail')
