e13b4594279b852744acad2623fe12c989f5f3e8
Otis Singfield <singfieldotis@gmail.com>
2021-04-05 20:14:00 -0400

    first API calls YAY

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 // User presented with a modal
 // Modal does age verification
 // If user is under age of 21, app runs without drink recipe
-// If uuser is over 21 app runs with drink recipe 
-// When app runs user is able to select movie based on genre, year it was made, and (?)
+// If user is over 21 app runs with drink recipe 
+// When app runs user is able to select movie based on genre, year it was made, and
 // Based on movie selections, a random food recipe and drink recipe are also given
 // Create a function to select the movie
 // Create a function to asign that movie a random value
@@ -13,3 +13,38 @@
 // selections can be made for food with user input as well
 
 // Possible upgrades: drink selecction based on users age
+
+
+var getMeal = function() {
+  var url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
+  fetch(url)
+  .then(function(response) {
+    response.json().then(function(mealChoice) {
+      console.log(mealChoice)
+    })
+  })
+}
+
+var getNonAlcDrink = function() {
+  var url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+  fetch(url)
+  .then(function(response) {
+    response.json().then(function(mealChoice) {
+      console.log(mealChoice)
+    })
+  })
+}
+
+var getAlcDrink = function() {
+  var url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+  fetch(url)
+  .then(function(response) {
+    response.json().then(function(mealChoice) {
+      console.log(mealChoice)
+    })
+  })
+}
+
+getMeal()
+getNonAlcDrink()
+getAlcDrink()
